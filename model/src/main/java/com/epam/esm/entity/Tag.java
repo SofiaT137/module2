d8a04c40a865fed794a2cc1c8196c2f4ceb2d571
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-04-02 21:50:57 +0300

    Add tag class

--- model/src/main/java/com/epam/esm/entity/Tag.java
+++ model/src/main/java/com/epam/esm/entity/Tag.java
@@ -1,2 +1,44 @@
-package com.epam.esm.entity;public class Tag {
+package com.epam.esm.entity;
+
+import java.util.Objects;
+
+public class Tag extends AbstractEntity{
+    private String name;
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public Tag(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tag)) return false;
+        if (!super.equals(o)) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(getName(), tag.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
