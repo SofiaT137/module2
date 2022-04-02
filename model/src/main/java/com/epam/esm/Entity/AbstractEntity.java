2955f4a692b0070a98095432b97b1d73ee2d114a
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-04-02 21:39:54 +0300

    Add AbstractEntity class

---
+++ model/src/main/java/com/epam/esm/Entity/AbstractEntity.java
@@ -0,0 +1,37 @@
+package com.epam.esm.Entity;
+
+import java.util.Objects;
+
+public abstract class AbstractEntity {
+
+    private Long id;
+
+    public AbstractEntity(){
+
+    }
+
+    public AbstractEntity(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AbstractEntity)) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
