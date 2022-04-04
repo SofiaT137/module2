37b3af39a1055765bfe6b4c0fa3e7f54ef431a7e
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-04-04 18:44:53 +0300

    Refactor GiftCertificateMapper

--- model/src/main/java/com/epam/esm/jbdc/mapper/GiftCertificateMapper.java
+++ model/src/main/java/com/epam/esm/jbdc/mapper/GiftCertificateMapper.java
@@ -1,26 +1,17 @@
 package com.epam.esm.jbdc.mapper;
 
 import com.epam.esm.entity.GiftCertificate;
-import org.springframework.jdbc.core.RowMapper;
-import static com.epam.esm.entity.GiftCertificateTableColumns.*;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class GiftCertificateMapper implements RowMapper<GiftCertificate> {
+
+public class GiftCertificateMapper implements ResultSetExtractor<GiftCertificate> {
+
     @Override
-    public GiftCertificate mapRow(ResultSet rs, int rowNum) throws SQLException {
-
-        GiftCertificate giftCertificate = new GiftCertificate();
-        giftCertificate.setId(rs.getLong(ID));
-        giftCertificate.setName(rs.getString(NAME));
-        giftCertificate.setPrice(rs.getDouble(PRICE));
-        giftCertificate.setDuration(rs.getInt(DURATION));
-        giftCertificate.setCreateDate(rs.getString(CREATE_DATE));
-        giftCertificate.setLastUpdateDate(rs.getString(LAST_UPDATE_DATE));
-
-
-        return giftCertificate;
-
+    public GiftCertificate extractData(ResultSet rs) throws SQLException, DataAccessException {
+        return null;
     }
 }
