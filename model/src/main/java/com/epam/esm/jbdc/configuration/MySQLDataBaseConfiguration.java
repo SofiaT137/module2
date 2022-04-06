9b04c78a7b5ff2b52ad0753354c732b909fb86e2
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-04-06 20:28:00 +0300

    Add changes for configuration file of model_layer

--- model/src/main/java/com/epam/esm/jbdc/configuration/MySQLDataBaseConfiguration.java
+++ model/src/main/java/com/epam/esm/jbdc/configuration/MySQLDataBaseConfiguration.java
@@ -1,10 +1,10 @@
 package com.epam.esm.jbdc.configuration;
 
 
-import com.mysql.cj.jdbc.MysqlDataSource;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,25 +14,28 @@ import java.sql.SQLException;
 
 
 @Configuration
+@ComponentScan("com.epam.esm")
 @PropertySource("classpath:dbConnection.properties")
 public class MySQLDataBaseConfiguration{
+
+    @Value("${DB_URL}")
+    private String url;
+    @Value("${USER}")
+    private String user;
+    @Value("${PASSWORD}")
+    private String password;
+    @Value("${DB_DRIVER}")
+    private String dbDriver;
+    @Value("${INITIAL_SIZE}")
+    private Integer poolSize;
 
     /**
      * This method creates bean of a DataSource object is the preferred means of getting a connection.
      * (The BasicDataSource object produces a standard Connection object for connecting to the physical data source.)
-     * @param url The database url
-     * @param user The database user
-     * @param password The database password
-     * @param dbDriver The database driver
-     * @param poolSize The database connection pool size
      * @return BasicDataSource object
      */
     @Bean
-    public DataSource dataSource(@Value("${DB_URL}") String url,
-                                         @Value("${USER}") String user,
-                                         @Value("${PASSWORD}") String password,
-                                         @Value("${DB_DRIVER}") String dbDriver,
-                                         @Value("${INITIAL_SIZE}") Integer poolSize){
+    public DataSource dataSource(){
         BasicDataSource basicDS = new BasicDataSource();
         basicDS.setUsername(user);
         basicDS.setPassword(password);
@@ -42,7 +45,6 @@ public class MySQLDataBaseConfiguration{
         return basicDS;
     }
 
-
     /**
      * This method creates JdbcTemplate object witch can be used for
      * @param dataSource The data source
@@ -50,7 +52,7 @@ public class MySQLDataBaseConfiguration{
      */
     @Bean
     public JdbcTemplate getJBDCTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
+        return new JdbcTemplate(dataSource());
     }
 
 
