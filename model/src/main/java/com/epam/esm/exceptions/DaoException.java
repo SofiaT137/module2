d082e1ef1ae85c530fb12312134ffca6497d59c1
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-04-06 20:25:36 +0300

    Add exception for model layer

---
+++ model/src/main/java/com/epam/esm/exceptions/DaoException.java
@@ -0,0 +1,34 @@
+package com.epam.esm.exceptions;
+
+public class DaoException extends Exception{
+
+    private String errorCode;
+
+    public DaoException() {
+    }
+
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DaoException (String message, String errorCode){
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public DaoException(Throwable cause) {
+        super(cause);
+    }
+
+    public DaoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
---
+++ model/src/main/java/com/epam/esm/exceptions/ExceptionErrorCode.java
@@ -0,0 +1,10 @@
+package com.epam.esm.exceptions;
+
+public interface ExceptionErrorCode {
+
+    String DATASOURCE_SAVING_ERROR = "40400";
+    String DATASOURCE_NOT_FOUND = "40401";
+    String DATASOURCE_NOT_FOUND_BY_PARAMETERS = "40402";
+    String DATASOURCE_NOT_FOUND_BY_NAME = "40403";
+    String DATASOURCE_NOT_FOUND_BY_ID = "40404";
+}
