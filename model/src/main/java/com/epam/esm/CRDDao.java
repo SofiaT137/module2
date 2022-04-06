a671bbe57e333c7960a65c5a8fc733d2e7646c0e
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-04-06 20:28:50 +0300

    Add changes for CRUD and CRD Dao classes

--- model/src/main/java/com/epam/esm/CRDDao.java
+++ model/src/main/java/com/epam/esm/CRDDao.java
@@ -1,10 +1,12 @@
 package com.epam.esm;
 
+import com.epam.esm.exceptions.DaoException;
+
 import java.util.List;
 
 public interface CRDDao<T>{
-    void insert(T entity);
-    T getById(long id);
-    List<T> getAll();
-    void deleteByID(long id);
+    void insert(T entity) throws DaoException;
+    T getById(long id) throws DaoException;
+    List<T> getAll() throws DaoException;
+    void deleteByID(long id) throws DaoException;
 }
--- model/src/main/java/com/epam/esm/CRUDDao.java
+++ model/src/main/java/com/epam/esm/CRUDDao.java
@@ -1,5 +1,7 @@
 package com.epam.esm;
 
+import com.epam.esm.exceptions.DaoException;
+
 public interface CRUDDao<T> extends CRDDao<T> {
-    void update(T entity);
+    void update(T entity) throws DaoException;
 }
