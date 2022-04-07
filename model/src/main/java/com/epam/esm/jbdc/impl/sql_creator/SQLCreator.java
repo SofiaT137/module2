7ab02764de38206e49eeaf59ac134af406cbd8c4
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-04-07 21:47:30 +0300

    Modify SQL creator

--- model/src/main/java/com/epam/esm/jbdc/impl/sql_creator/SQLCreator.java
+++ model/src/main/java/com/epam/esm/jbdc/impl/sql_creator/SQLCreator.java
@@ -1,85 +1,104 @@
 package com.epam.esm.jbdc.impl.sql_creator;
 
-import com.epam.esm.TagDao;
-import com.epam.esm.exceptions.DaoException;
-import com.epam.esm.jbdc.impl.TagDaoImpl;
-
 import java.util.Map;
+
 import static com.epam.esm.jbdc.sql_queries.GiftCertificateQueries.*;
+import static com.epam.esm.entity.GiftCertificateTableColumns.*;
 
 public class SQLCreator {
 
-    private static final String TAG_NAME = "tagName";
+    private static final String TAG_NAME = "tag_name";
     private static final String PART_OF_CERTIFICATE_NAME = "partName";
     private static final String PART_OF_CERTIFICATE_DESCRIPTION = "partDescription";
-    private static final String SORT_BY_NAME = "sortByName";
-    private static final String SORT_BY_DATE = "sortByCreationDate";
+    private static final String SORT_BY_NAME = "sortByNameASC";
+    private static final String SORT_BY_DATE = "sortByCreationDateASC";
     private static final String SORT_BY_NAME_DESC = "sortByNameDESC";
     private static final String SORT_BY_DATE_DESC = "sortByCreationDateDESC";
 
 
-    public String createGetCertificateQuery(Map<String,String> queryParts){
+    public String createGetCertificateQuery(Map<String, String> queryParts) {
 
         StringBuilder query = new StringBuilder(GET_GIFT_CERTIFICATES);
 
-        for (Map.Entry<String, String> addToQuery : queryParts.entrySet()) {
-            String key = addToQuery.getKey();
-            String value = addToQuery.getValue();
-            if(key.equals(TAG_NAME)) {
-                 addJoinQuery(query, value);
+        StringBuilder joinPart = new StringBuilder();
+        StringBuilder partSearchPart = new StringBuilder();
+        StringBuilder orderPart = new StringBuilder();
+
+        for (Map.Entry<String, String> mapWithQueryParts : queryParts.entrySet()) {
+            String key = mapWithQueryParts.getKey();
+            String value = mapWithQueryParts.getValue();
+            if (key.equals(TAG_NAME)) {
+                addJoinQuery(joinPart, value);
             }else if (key.equals(PART_OF_CERTIFICATE_NAME)) {
-                 addWhere(query, "name", value);
-             }else if(key.equals(PART_OF_CERTIFICATE_DESCRIPTION)){
-                addWhere(query,"description",value);
-             }else if (key.equals(SORT_BY_NAME)){
-                addSortASC(query,"name",value);
-             }else if (key.equals(SORT_BY_DATE)){
-                 addSortASC(query,"date",value);
-             }else if (key.equals(SORT_BY_NAME_DESC)){
-                addSortDESC(query,"name",value);
-             }else if(key.equals(SORT_BY_DATE_DESC)) {
-                addSortDESC(query,"date",value);
+                addPartSearchParameter(partSearchPart,NAME,value);
+            }else if (key.equals(PART_OF_CERTIFICATE_DESCRIPTION)) {
+                addPartSearchParameter(partSearchPart,DESCRIPTION,value);
+            }else if (key.equals(SORT_BY_NAME)){
+                addSort(orderPart,value,false);
+            }else if (key.equals(SORT_BY_DATE)){
+                addSort(orderPart,value,false);
+            }else if (key.equals(SORT_BY_NAME_DESC)){
+                addSort(orderPart,value,true);
+            }else if (key.equals(SORT_BY_DATE_DESC)){
+                addSort(orderPart,value,true);
             }
         }
-        return query.toString();
+
+        StringBuilder checkQuery = query.append(joinPart);
+
+        return getFinalQuery(checkQuery,checkForDoubleWhere(checkQuery,partSearchPart),orderPart);
     }
 
-    private void addJoinQuery(StringBuilder query, String tagName){
-        TagDao tagDao = new TagDaoImpl();
-        try {
-            query.append(JOIN_BY_TAG_ID).append(tagDao.getTagByName(tagName).getId());
-        } catch (DaoException exception) {
-           exception.getErrorCode();
-        }
-    }
-
-    private void addWhere(StringBuilder query, String parameter,String value){
-        if (query.toString().contains("WHERE")) {
-            query.append(" AND ");
-        }else {
-            query.append("WHERE");
-        }
-        query.append(parameter).append(" LIKE '%").append(value).append("%'");
-    }
-
-    private void addSortASC(StringBuilder query, String parameter,String value){
-        if (query.toString().contains("ORDER BY")) {
-            query.append(", ");
+    private void addJoinQuery(StringBuilder joinPart, String tagName) {
+        if (joinPart.toString().contains(WHERE)) {
+            joinPart.append(AND);
         } else {
-            query.append(" ORDER BY ");
+            joinPart.append(JOIN_BY_TAG_ID).append(WHERE);
         }
-        query.append(parameter).append(" ").append(value);
+        joinPart.append(TAG_NAME).append(EQUALS_QUOTE).append(tagName).append(QUOTE);
     }
 
-    private void addSortDESC(StringBuilder query, String parameter,String value){
-        if (query.toString().contains("ORDER BY")) {
-            query.append(", ");
+    private void addPartSearchParameter(StringBuilder query, String parameter, String value) {
+        if (query.toString().contains(WHERE)) {
+            query.append(AND);
         } else {
-            query.append(" ORDER BY ");
+            query.append(WHERE);
         }
-        query.append(parameter).append(" ").append(value).append("DESC");
+        query.append(parameter).append(LIKE_QUOTE_PERCENT).append(value).append(PERCENT_QUOTE);
     }
 
+    private void addSort(StringBuilder query, String value, boolean isDESC){
+        if (query.toString().contains(ORDER_BY)) {
+            query.append(COMA);
+        } else {
+            query.append(ORDER_BY);
+        }
+        query.append(value);
+        if (isDESC){
+            query.append(DESC);
+        }
+    }
+
+    private StringBuilder checkForDoubleWhere(StringBuilder currentSB,StringBuilder destinationSB){
+        if (currentSB.toString().contains(WHERE)){
+           return new StringBuilder(destinationSB.toString().replace(WHERE,AND));
+        }
+        return destinationSB;
+    }
+
+    private String getFinalQuery(StringBuilder...sb){
+        if (sb.length == 0){
+            return "";
+        }
+        StringBuilder finalString = new StringBuilder();
+        for (StringBuilder sbEntity:
+             sb) {
+            finalString.append(sbEntity);
+            finalString.append(" \n");
+        }
+        finalString.deleteCharAt(finalString.length() - 2);
+        return finalString.toString();
+    }
 }
 
 
