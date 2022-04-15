b5be590d8f57e9d8d58f8d1c433321e9116b1b19
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-04-15 11:30:51 +0300

    Make class Validator abstract

--- service/src/main/java/com/epam/esm/validator/Validator.java
+++ service/src/main/java/com/epam/esm/validator/Validator.java
@@ -3,7 +3,7 @@ package com.epam.esm.validator;
 import com.epam.esm.exceptions.ServiceException;
 import static com.epam.esm.exceptions.ExceptionErrorCode.INCORRECT_ID;
 
-public class Validator {
+public abstract class Validator {
 
     private static final int MIN_ID = 1;
     private static final String INCORRECT_ID_EXCEPTION_MESSAGE = "This id is incorrect!";
