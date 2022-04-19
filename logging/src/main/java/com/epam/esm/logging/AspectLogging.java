98498fccfd388613192ca48008436189447ba2a7
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-04-19 10:17:41 +0300

    Add javadocs for logging layer

--- logging/src/main/java/com/epam/esm/logging/AspectLogging.java
+++ logging/src/main/java/com/epam/esm/logging/AspectLogging.java
@@ -7,37 +7,66 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * Aspect class for Logger
+ */
 @Component
 @Aspect
 public class AspectLogging {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AspectLogging.class);
+    private static final String EXECUTION_ALL_SERVICE_METHODS = "execution(* com.epam.esm.service.*.*(..))";
+    private static final String EXECUTION_ALL_MODEL_METHODS = "execution(* com.epam.esm.jbdc.*.*(..))";
+    private static final String SERVICE_POINTCUT_REFERENCE_NAME = "allServiceMethods()";
+    private static final String MODEL_POINTCUT_REFERENCE_NAME = "allModelMethods()";
+    private static final String SERVER_LAYER_METHOD_NAME = "Service layer, Method name: ";
+    private static final String MODEL_LAYER_METHOD_NAME = "Model layer, Method name: ";
+    private static final String SERVER_LAYER_METHOD_THROWS_AN_EXCEPTION = "Method from Service layer throws an exception: ";
+    private static final String MODEL_LAYER_METHOD_THROWS_AN_EXCEPTION = "Method from Model layer throws an exception: ";
+    private static final String STARTS = " starts";
+    private static final String EXCEPTION = "exception";
 
-    @Pointcut("execution(* com.epam.esm.service.*.*(..))")
+    @Pointcut(EXECUTION_ALL_SERVICE_METHODS)
     private void allServiceMethods() {
     }
 
-    @Pointcut("execution(* com.epam.esm.jbdc.*.*(..))")
+    @Pointcut(EXECUTION_ALL_MODEL_METHODS)
     private void allModelMethods() {
     }
 
-    @Before("allServiceMethods()")
+    /**
+     * Before service methods' advice method
+     * @param joinPoint JoinPoint joinPoint
+     */
+    @Before(SERVICE_POINTCUT_REFERENCE_NAME)
     public void beforeServiceMethodsAdvice(JoinPoint joinPoint) {
-        LOGGER.info("Service layer, Method name: " + joinPoint.getSignature().getName() + " starts");
+        LOGGER.info(SERVER_LAYER_METHOD_NAME + joinPoint.getSignature().getName() + STARTS);
     }
 
-    @AfterThrowing(pointcut = "allServiceMethods()", throwing = "exception")
+    /**
+     * After throwing an exception service methods' advice method
+     * @param exception Throwable exception
+     */
+    @AfterThrowing(pointcut = SERVICE_POINTCUT_REFERENCE_NAME, throwing = EXCEPTION)
     public void afterThrowingServiceMethodsAdvice(Throwable exception) {
-        LOGGER.error("Method from Service layer throws an exception: " + exception);
+        LOGGER.error(SERVER_LAYER_METHOD_THROWS_AN_EXCEPTION + exception);
     }
 
-    @Before("allModelMethods()")
+    /**
+     * Before model methods' advice method
+     * @param joinPoint JoinPoint joinPoint
+     */
+    @Before(MODEL_POINTCUT_REFERENCE_NAME)
     public void beforeModelMethodsAdvice(JoinPoint joinPoint) {
-        LOGGER.info("Model layer, Method name: " + joinPoint.getSignature().getName() + " starts");
+        LOGGER.info(MODEL_LAYER_METHOD_NAME + joinPoint.getSignature().getName() + STARTS);
     }
 
-    @AfterThrowing(pointcut = "allModelMethods()", throwing = "exception")
+    /**
+     * After throwing an exception model methods' advice method
+     * @param exception Throwable exception
+     */
+    @AfterThrowing(pointcut = MODEL_POINTCUT_REFERENCE_NAME, throwing = EXCEPTION)
     public void afterThrowingModelMethodsAdvice(Throwable exception) {
-        LOGGER.error("Method from Model layer throws an exception: " + exception);
+        LOGGER.error(MODEL_LAYER_METHOD_THROWS_AN_EXCEPTION + exception);
     }
 }
--- logging/src/main/java/com/epam/esm/logging_configuration/LoggerConfiguration.java
+++ logging/src/main/java/com/epam/esm/logging_configuration/LoggerConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+/**
+ * LoggerConfiguration class presents aspect configuration class.
+ */
 @Configuration
 @ComponentScan("com.epam.esm")
 @EnableAspectJAutoProxy
