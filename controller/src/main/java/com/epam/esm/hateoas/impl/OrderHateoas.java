38eaf546a57dc430e9d4f183bf70e6f4b28657b0
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-05-15 17:57:07 +0300

    Refactor OrderHateoas class

--- controller/src/main/java/com/epam/esm/hateoas/impl/OrderHateoas.java
+++ controller/src/main/java/com/epam/esm/hateoas/impl/OrderHateoas.java
@@ -2,6 +2,7 @@ package com.epam.esm.hateoas.impl;
 
 import com.epam.esm.controllers.GiftCertificateController;
 import com.epam.esm.controllers.OrderController;
+import com.epam.esm.controllers.TagController;
 import com.epam.esm.dto.OrderDto;
 import com.epam.esm.hateoas.Hateoas;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,7 @@ public class OrderHateoas implements Hateoas<OrderDto> {
 
     private static final Class<OrderController> ORDER_CONTROLLER = OrderController.class;
     private static final Class<GiftCertificateController> GIFT_CERTIFICATE_CONTROLLER = GiftCertificateController.class;
+    private static final Class<TagController> TAG_CONTROLLER = TagController.class;
 
     @Override
     public void addLinks(OrderDto entity) {
@@ -21,5 +23,9 @@ public class OrderHateoas implements Hateoas<OrderDto> {
         entity.getGiftCertificateDto().forEach(
                 giftCertificateDto -> giftCertificateDto.add(linkTo(methodOn(GIFT_CERTIFICATE_CONTROLLER)
                         .getCertificateByID(giftCertificateDto.getId())).withSelfRel()));
+        entity.getGiftCertificateDto().forEach(
+                giftCertificateDto -> giftCertificateDto.getTags().forEach(tagDto -> tagDto
+                        .add(linkTo(methodOn(TAG_CONTROLLER)
+                        .getTagById(tagDto.getId())).withSelfRel())));
     }
 }
