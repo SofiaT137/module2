7ce4a061d7b7dae2143e959e1de5d0876b916950
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-05-17 10:27:02 +0300

    Add serializible to the abstract entity

--- model/src/main/java/com/epam/esm/entity/AbstractEntity.java
+++ model/src/main/java/com/epam/esm/entity/AbstractEntity.java
@@ -1,11 +1,15 @@
 package com.epam.esm.entity;
 
 import com.epam.esm.audit.EntityListener;
-import org.apache.tomcat.jni.Local;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.Objects;
 
 /**
@@ -14,7 +18,7 @@ import java.util.Objects;
  */
 @MappedSuperclass
 @EntityListeners(EntityListener.class)
-public abstract class AbstractEntity<K> {
+public abstract class AbstractEntity<K extends Serializable> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
