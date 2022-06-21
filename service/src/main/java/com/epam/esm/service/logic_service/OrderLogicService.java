3ffa0d0c5ec9db6cd4dbb4018b686be524ff13d2
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-06-21 12:31:28 +0300

    Fix bug

--- service/src/main/java/com/epam/esm/service/logic_service/OrderLogicService.java
+++ service/src/main/java/com/epam/esm/service/logic_service/OrderLogicService.java
@@ -59,6 +59,7 @@ public class OrderLogicService implements OrderService<Order> {
     }
 
     private static final String NO_ORDER_WITH_THAT_ID_EXCEPTION_MESSAGE = "noOrderWithThatId";
+    private static final String NO_ROLE_WITH_THAT_ID_EXCEPTION_MESSAGE = "noRoleWithThatId";
     private static final String CANNOT_INSERT_THIS_ORDER_EXCEPTION_MESSAGE = "cannotInsertThisOrder!";
     private static final String NO_USER_WITH_THAT_ID_EXCEPTION_MESSAGE = "noUserWithId";
     private static final String NO_GIFT_CERTIFICATE_WITH_THAT_ID_EXCEPTION_MESSAGE = "noGiftCertificateWithThatId";
@@ -115,13 +116,18 @@ public class OrderLogicService implements OrderService<Order> {
     public Page<Order> getAll(int pageNumber, int pageSize) {
         User currentUser = getUser();
         Optional<Role> role = roleRepository.findById(1L);
-        if (currentUser.getRoles().contains(role.get())){
-            return pagination.checkHasContent(orderRepository.findAll(PageRequest.of(pageNumber,pageSize)));
-        }else{
+        if (!role.isPresent()) {
+            throw new NoSuchEntityException(NO_ROLE_WITH_THAT_ID_EXCEPTION_MESSAGE);
+        }
+        if (currentUser.getRoles().contains(role.get())) {
+            return pagination.checkHasContent(orderRepository.findAll(PageRequest.of(pageNumber, pageSize)));
+        } else {
             return pagination.checkHasContent(orderRepository.findAllOrderWhereUserId(currentUser.getId(),
-                    PageRequest.of(pageNumber,pageSize)));
+                    PageRequest.of(pageNumber, pageSize)));
         }
     }
+
+
 
     @Override
     public Page<Order> ordersByUserId(long userId, int pageNumber,int pageSize){
