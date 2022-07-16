24063faab1559835d04c859c45e079bbd4ce5630
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-07-16 10:06:13 +0300

    Allow CORS

--- controller/src/main/java/com/epam/esm/SpringBootApplicationRunner.java
+++ controller/src/main/java/com/epam/esm/SpringBootApplicationRunner.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * SpringBootApplicationRunner is a main configuration class helps to run SpringBoot Application
@@ -20,5 +23,18 @@ public class SpringBootApplicationRunner extends SpringBootServletInitializer {
         ConfigurableApplicationContext context = SpringApplication.run(SpringBootApplicationRunner.class,args);
         DispatcherServlet dispatcherServlet = (DispatcherServlet) context.getBean(DISPATCHER_SERVLET);
         dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET","POST","PUT","DELETE")
+                        .allowedHeaders("*")
+                        .allowedOrigins("*");
+            }
+        };
     }
 }
