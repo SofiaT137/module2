65bb0c752bbedbeda12faa0925e9f42bba26e616
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-08-03 10:44:22 +0300

    The final CORS filter settings

--- controller/src/main/java/com/epam/esm/SpringBootApplicationRunner.java
+++ controller/src/main/java/com/epam/esm/SpringBootApplicationRunner.java
@@ -6,6 +6,10 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 /**
  * SpringBootApplicationRunner is a main configuration class helps to run SpringBoot Application
@@ -17,8 +21,18 @@ public class SpringBootApplicationRunner extends SpringBootServletInitializer {
     private static final String DISPATCHER_SERVLET = "dispatcherServlet";
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(SpringBootApplicationRunner.class,args);
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootApplicationRunner.class, args);
         DispatcherServlet dispatcherServlet = (DispatcherServlet) context.getBean(DISPATCHER_SERVLET);
         dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
